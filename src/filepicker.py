daa2dc586723015dab53224cf4eb2d57f2a70e10
yh-nr <ynaru911@gmail.com>
2023-08-17 08:12:43 +0900

    0817

--- src/filepicker.py
+++ src/filepicker.py
@@ -4,17 +4,16 @@ import json
 from . import config_manager
 
 # Request code for selecting a JSON document.
-PICK_JSON_FILE = 42  # JSONファイルを選択するためのリクエストコードを定義します。
+PICK_JSON_FILE = 42
+RESULT_OK = -1  # Define a constant for RESULT_OK
 
-# JavaクラスをPythonで利用できるようにする
+# Java classes for Python
 try:
     Uri = autoclass('android.net.Uri')
     Intent = autoclass('android.content.Intent')
     PythonActivity = autoclass('org.kivy.android.PythonActivity')
-except:
-    print('autoclassでエラーでたよ！！！！！！')
-    print('autoclassでエラーでたよ！！！！！！')
-    print('autoclassでエラーでたよ！！！！！！')
+except Exception as e:
+    print("Failed to import Java classes:", e)
 
 class ActivityResultEvent(PythonJavaClass):
     __javainterfaces__ = ['org/kivy/android/ActivityResultListener']
@@ -26,86 +25,63 @@ class ActivityResultEvent(PythonJavaClass):
 
     @java_method('(IILandroid/content/Intent;)V')
     def onActivityResult(self, requestCode, resultCode, intent):
-        # ファイルピッカーからの結果を処理するメソッド
         if requestCode == PICK_JSON_FILE:
-            if resultCode == -1:  # RESULT_OK
-                uri = intent.getData()  # 選択されたファイルのURIを取得します。
+            if resultCode == RESULT_OK:
+                uri = intent.getData()
                 json_text = read_json_file(uri)
                 json_data = json.loads(json_text)
-                self.callback(json_data)  # コールバック関数を呼び出します
+                self.callback(json_data)
 
 def open_file(picker_initial_uri):
-    # ファイルピッカーを開くための関数
     intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
     intent.addCategory(Intent.CATEGORY_OPENABLE)
-    intent.setType("application/json")  # JSONファイルのみを選択できるように設定します。
+    intent.setType("application/json")
     current_activity = PythonActivity.mActivity
     current_activity.startActivityForResult(intent, PICK_JSON_FILE)
 
 def read_json_file(uri):
-    # URIからJSONファイルの内容を読み取る関数
     content_resolver = PythonActivity.mActivity.getContentResolver()
     stream = content_resolver.openInputStream(uri)
     
     json_text = ''
     buffer = bytearray(1024)
-    while True:
-        length = stream.read(buffer)
-        if length == -1:
-            break
-        json_text += buffer[:length].decode('utf-8')
+    try:
+        while True:
+            length = stream.read(buffer)
+            if length == -1:
+                break
+            json_text += buffer[:length].decode('utf-8')
+    finally:
+        stream.close()
     
-    stream.close()
-    
-    json_data = json.loads(json_text)  # JSONテキストをPythonのデータ構造に変換します。
-    process_json_data(json_data)  # Pythonのデータ構造を別の関数に渡します。
+    return json_text
 
-
-
-
-# from jnius import autoclass  # Pyjniusのautoclass関数をインポートします。これを使用してJavaのクラスをPythonからアクセスできるようにします。
-
-# # JavaクラスをPythonで利用できるようにする
-# Uri = autoclass('android.net.Uri')  # AndroidのUriクラスをPythonで利用できるようにします。
-# Intent = autoclass('android.content.Intent')  # AndroidのIntentクラスをPythonで利用できるようにします。
-# DocumentsContract = autoclass('android.provider.DocumentsContract')  # AndroidのDocumentsContractクラスをPythonで利用できるようにします。
-
-# # Request code for selecting a PDF document.
-# PICK_PDF_FILE = 2  # PDFファイルを選択するためのリクエストコードを定義します。このコードは後で結果を受け取る際に使用します。
-
-# def open_file(picker_initial_uri):  # open_fileという関数を定義します。この関数は、ファイルピッカーを開くために使用されます。
-#     intent = Intent(Intent.ACTION_OPEN_DOCUMENT)  # OPEN_DOCUMENTアクションを持つ新しいIntentオブジェクトを作成します。これにより、ファイルピッカーが開きます。
-#     intent.addCategory(Intent.CATEGORY_OPENABLE)  # カテゴリとしてOPENABLEを追加します。これにより、選択可能なファイルのみが表示されます。
-#     intent.setType("application/pdf")  # ファイルピッカーで表示されるファイルのタイプをPDFに設定します。
-
-#     # Optionally, specify a URI for the file that should appear in the
-#     # system file picker when it loads.
-#     intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, picker_initial_uri)  # オプションとして、ファイルピッカーが開いたときに表示される初期のURIを設定します。
-
-#     # PythonActivityはKivyのアプリケーションで現在実行中のActivityを参照するためのクラスです
-#     PythonActivity = autoclass('org.kivy.android.PythonActivity')  # Kivyアプリケーションで現在実行中のAndroidのActivityを参照するためのPythonActivityクラスを取得します。
-#     current_activity = PythonActivity.mActivity  # 現在実行中のActivityのインスタンスを取得します。
-
-#     current_activity.startActivityForResult(intent, PICK_PDF_FILE)  # startActivityForResultメソッドを使用して、ファイルピッカーを開き、後で結果を受け取ることができるようにします。
-
-# # Example usage
-# picker_initial_uri = Uri.parse("content://downloads/public_downloads")  # ファイルピッカーが開いたときに表示される初期のURIを設定します。ここではダウンロードフォルダを指定しています。
-# open_file(picker_initial_uri)  # 上で定義したopen_file関数を呼び出して、ファイルピッカーを開きます。
-
-
-# この関数を外部から呼び出す
-def load_json_4android():    
-    picker_initial_uri = Uri.parse("file:///sdcard/")
-    open_file(picker_initial_uri)  # ファイルピッカーを開く関数を呼び出します。
-
-    pass
-
+# def load_json_4android():    
+#     event = ActivityResultEvent(callback_function)
+#     picker_initial_uri = Uri.parse("file:///sdcard/")
+#     open_file(picker_initial_uri)
 
 def process_json_data(data):
-    # JSONデータを処理する関数
-    config_manager.save_config_to_file(config_manager.SETTINGS_FILE, data)
-    print(data)  # ここではJSONデータを単に出力していますが、実際にはこのデータを使用して何らかの処理を行います。
+    try:
+        config_manager.save_config_to_file(config_manager.SETTINGS_FILE, data)
+        print(data)
+    except Exception as e:
+        print("Failed to process JSON data:", e)
 
+# def callback_function(json_data):
+#     try:
+#         process_json_data(json_data)
+#     except json.JSONDecodeError as e:
+#         print("Failed to decode JSON:", e)
+
+def load_json_4android():    
+    def callback_function(json_data):
+        try:
+            process_json_data(json_data)
+        except json.JSONDecodeError as e:
+            print("Failed to decode JSON:", e)
     
-# KivyのPythonActivityにActivityResultListenerを追加します。
-PythonActivity.addActivityResultListener(process_json_data())
+    event = ActivityResultEvent(callback_function)
+    
+    picker_initial_uri = Uri.parse("file:///sdcard/")
+    open_file(picker_initial_uri)
